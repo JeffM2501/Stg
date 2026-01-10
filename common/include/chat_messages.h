0d6bed69f053420660536ddc879ffcecc81ec399
Jeffery Myers <JeffM2501@gmail.com>
2026-01-09 16:56:14 -0800

    group ID stuff

--- common/include/chat_messages.h
+++ common/include/chat_messages.h
@@ -8,10 +8,10 @@ namespace Pack
     class ServerAddChatUser : public MessagePackBuffer
     {
 	public:
-		ServerAddChatUser(uint32_t userId, std::string_view userName)
+		ServerAddChatUser(uint32_t userId, std::string_view userName, uint32_t groupId = 0)
 		{
 			Channel = NetworkChannelIDs::Chat;
-			AllocatePacket(GetBufferWriteSize(userName.size()) + sizeof(uint32_t));
+			AllocatePacket(GetBufferWriteSize(userName.size()) + sizeof(uint32_t) + sizeof(uint32_t));
 			WriteTypeID(MessageIDS::ServerAddChatUser);
 			SetUserId(userId);
 			SetUserName(userName);
@@ -22,9 +22,14 @@ namespace Pack
 			WriteValue<uint32_t>(id, 0);
 		}
 
+		void SetGroupId(uint32_t id)
+		{
+			WriteValue<uint32_t>(id, 4);
+		}
+
 		void SetUserName(std::string_view userName)
 		{
-			WriteBufferValue(userName.data(), userName.size(), 4);
+			WriteBufferValue(userName.data(), userName.size(), 8);
 		}
     };
 
@@ -48,13 +53,14 @@ namespace Pack
     class ServerTextMessage : public MessagePackBuffer
     {
     public:
-        ServerTextMessage(std::string_view message, uint32_t senderId = 0, bool isPrivate = false)
+        ServerTextMessage(std::string_view message, uint32_t senderId = 0, uint32_t groupID = 0, bool isPrivate = false)
         {
             Channel = NetworkChannelIDs::Chat;
-            AllocatePacket(GetBufferWriteSize(message.size()) + sizeof(uint32_t) + sizeof(uint8_t));
+            AllocatePacket(GetBufferWriteSize(message.size()) + sizeof(uint32_t) + sizeof(uint32_t) + sizeof(uint8_t));
             WriteTypeID(MessageIDS::ServerTextMessage);
 
             SetSenderId(senderId);
+			SetGroupId(groupID);
             SetMessage(message);
 			SetIsPrivate(isPrivate);
         }
@@ -64,16 +70,50 @@ namespace Pack
 			WriteValue<uint32_t>(id, 0);
 		}
 
+		void SetIsPrivate(bool isPrivate)
+		{
+			WriteValue<uint8_t>(isPrivate ? 1 : 0, 4);
+		}
+
+		void SetGroupId(uint32_t groupID)
+		{
+			WriteValue<uint32_t>(groupID, 8);
+		}
+
         void SetMessage(std::string_view message)
         {
-            WriteBufferValue(message.data(), message.size(), 5);
+            WriteBufferValue(message.data(), message.size(), 9);
         }
+    };
+
+	class SetChatGroupInfo : public MessagePackBuffer
+	{
+	public:
+		SetChatGroupInfo(uint32_t groupId, std::string_view name, bool isPrivate = false)
+		{
+			Channel = NetworkChannelIDs::Chat;
+			AllocatePacket(GetBufferWriteSize(name.size()) + sizeof(uint32_t) + sizeof(uint8_t));
+			WriteTypeID(MessageIDS::SetChatGroupInfo);
+			SetGroupId(groupId);
+			SetIsPrivate(isPrivate);
+			SetName(name);
+		}
+
+		void SetGroupId(uint32_t id)
+		{
+			WriteValue<uint32_t>(id, 0);
+		}
 
 		void SetIsPrivate(bool isPrivate)
 		{
 			WriteValue<uint8_t>(isPrivate ? 1 : 0, 4);
 		}
-    };
+
+		void SetName(std::string_view name)
+		{
+			WriteBufferValue(name.data(), name.size(), 5);
+		}
+	};
 }
 
 namespace Unpack
@@ -86,11 +126,13 @@ namespace Unpack
 		int GetProcessingChannel() override { return RouteID::ChatHandler; }
 
         uint32_t UserID = 0;
+		uint32_t GroupID = 0;
 		std::string_view Name;
 
         ServerAddChatUser(ENetPacket* packet) : MessageUnpackBuffer(packet)
 		{
 			Read(UserID);
+			Read(GroupID);
 			Read(Name);
 		}
 	};
@@ -118,12 +160,15 @@ namespace Unpack
         int GetProcessingChannel() override { return RouteID::ChatHandler; }
 
         uint32_t SenderId = 0;
+		uint64_t GroupId = 0;
         std::string_view Message;
 		bool IsPrivate = false;
 
         ServerTextMessage(ENetPacket* packet) : MessageUnpackBuffer(packet)
         {
             Read(SenderId);
+			Read(GroupId);
+
 			uint8_t temp = 0;
 			Read(temp);
 			IsPrivate = temp != 0;
--- common/include/message_ids.h
+++ common/include/message_ids.h
@@ -8,6 +8,7 @@ namespace MessageIDS
     static constexpr uint64_t ServerTextMessage = Hashes::CRC64Str("ServerTextMessage");
     static constexpr uint64_t ServerAddChatUser = Hashes::CRC64Str("ServerAddChatUser");
 	static constexpr uint64_t ServerRemoveChatUser = Hashes::CRC64Str("ServerRemoveChatUser");
+	static constexpr uint64_t SetChatGroupInfo = Hashes::CRC64Str("SetChatGroupInfo");
 
     static constexpr uint64_t WorldInfo = Hashes::CRC64Str("WorldInfo");
 }
