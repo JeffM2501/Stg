615734e124c4d6606d9f4df53b3a4807bb5c8ea7
Jeffery Myers <JeffM2501@gmail.com>
2026-01-13 17:19:35 -0800

    hook up premake

--- codegen/Program.cs
+++ codegen/Program.cs
@@ -1,123 +1,131 @@
 ﻿using System;
-using System.Diagnostics.CodeAnalysis;
+using System.Collections.Generic;
 using System.IO;
 
-using codegen;
+#nullable enable
 
-if (args.Length < 3)
+namespace codegen
 {
-    Console.WriteLine("Missing args");
-    return;
-}
-
-string OutputType = args[0];
-
-string SchemaDir = args[1];
-if (string.IsNullOrEmpty(SchemaDir) || !Directory.Exists(SchemaDir))
-{
-    Console.WriteLine("Input Dir " + SchemaDir + " does not exit");
-    return;
-}
-
-string OutputDir = args[2];
-if (string.IsNullOrEmpty(OutputDir) || !Directory.Exists(OutputDir))
-{
-    Console.WriteLine("Output Dir " + OutputDir + " does not exit");
-    return;
-}
-
-
-
-foreach (var file in Directory.GetFiles(SchemaDir, "*.schema"))
-{
-    List<ClassInfo> classes = new List<ClassInfo>();
-
-    string outputFileName = Path.GetFileNameWithoutExtension(file);
-
-    var reader = File.OpenText(file);
-
-    string NamespaceString = string.Empty;
-
-    ClassInfo? currentClass = null;
-
-    var text = reader.ReadToEnd();
-    text = text.Replace("\r", string.Empty);
-
-    foreach ( var line in text.Split(new char[] { '\n' }))
+    class Program
     {
-        if (string.IsNullOrEmpty(line))
-            continue;
-
-        var newLine = line.Trim();
-
-        if (string.IsNullOrEmpty(newLine))
-            continue;
-
-        var parts = newLine.Split(new char[] { ' ', '\t' },2);
-
-        if (parts.Length < 1)
-            continue;
-
-        var keyword = parts[0].ToLower();
-        if (keyword == "namespace")
+        static void Main(string[] args)
         {
-            NamespaceString = parts[1];
-        }
-        else
-        {
-            if (currentClass  == null)
+
+            if (args.Length < 3)
             {
-                currentClass = new ClassInfo();
-
-                currentClass.ClassType = parts[0];
-                currentClass.Name = parts[1];
+                Console.WriteLine("Missing args");
+                return;
             }
-            else
+
+            string OutputType = args[0];
+
+            string SchemaDir = args[1];
+            if (string.IsNullOrEmpty(SchemaDir) || !Directory.Exists(SchemaDir))
             {
-                if (keyword == "}")
+                Console.WriteLine("Input Dir " + SchemaDir + " does not exit");
+                return;
+            }
+
+            string OutputDir = args[2];
+            if (string.IsNullOrEmpty(OutputDir) || !Directory.Exists(OutputDir))
+            {
+                Console.WriteLine("Output Dir " + OutputDir + " does not exit");
+                return;
+            }
+
+            foreach (var file in Directory.GetFiles(SchemaDir, "*.schema"))
+            {
+                List<ClassInfo> classes = new List<ClassInfo>();
+
+                string outputFileName = Path.GetFileNameWithoutExtension(file);
+
+                var reader = File.OpenText(file);
+
+                string NamespaceString = string.Empty;
+
+                ClassInfo? currentClass = null;
+
+                var text = reader.ReadToEnd();
+                text = text.Replace("\r", string.Empty);
+
+                foreach (var line in text.Split(new char[] { '\n' }))
                 {
-                    classes.Add(currentClass);
-                    currentClass = null;
-                }
-                else if (keyword != "{")
-                {
-                    if (keyword.StartsWith("["))
+                    if (string.IsNullOrEmpty(line))
+                        continue;
+
+                    var newLine = line.Trim();
+
+                    if (string.IsNullOrEmpty(newLine))
+                        continue;
+
+                    var parts = newLine.Split(new char[] { ' ', '\t' }, 2);
+
+                    if (parts.Length < 1)
+                        continue;
+
+                    var keyword = parts[0].ToLower();
+                    if (keyword == "namespace")
                     {
-                        currentClass.Metadata.Add(keyword, parts[1]);
+                        NamespaceString = parts[1];
                     }
                     else
                     {
-                        FieldInfo field = new FieldInfo();
-                        field.FieldTypename = parts[0];
-                        string name = parts[1].TrimEnd(';');
-                        if (name.Contains("="))
+                        if (currentClass == null)
                         {
-                            var nameParts = name.Split(new char[] { '=' });
-                            name = nameParts[0].Trim();
-                            field.DefaultValue = nameParts[1].Trim();
+                            currentClass = new ClassInfo();
+
+                            currentClass.ClassType = parts[0];
+                            currentClass.Name = parts[1];
                         }
+                        else
+                        {
+                            if (keyword == "}")
+                            {
+                                classes.Add(currentClass);
+                                currentClass = null;
+                            }
+                            else if (keyword != "{")
+                            {
+                                if (keyword.StartsWith("["))
+                                {
+                                    currentClass.Metadata.Add(keyword, parts[1]);
+                                }
+                                else
+                                {
+                                    FieldInfo field = new FieldInfo();
+                                    field.FieldTypename = parts[0];
+                                    string name = parts[1].TrimEnd(';');
+                                    if (name.Contains("="))
+                                    {
+                                        var nameParts = name.Split(new char[] { '=' });
+                                        name = nameParts[0].Trim();
+                                        field.DefaultValue = nameParts[1].Trim();
+                                    }
 
-                        field.Name = name;
+                                    field.Name = name;
 
-                        currentClass.Fields.Add(field);
+                                    currentClass.Fields.Add(field);
+                                }
+                            }
+                        }
                     }
                 }
+
+
+                string outPath = Path.Combine(OutputDir, outputFileName + ".h");
+
+                switch (OutputType)
+                {
+                    case "c++":
+                        CPPGenerator.Generate(classes, NamespaceString, outPath);
+                        break;
+
+                    default:
+                        Console.WriteLine("Output mode " + OutputType + " is unknown");
+                        break;
+                }
             }
+
         }
     }
-
-
-    string outPath = Path.Combine(OutputDir, outputFileName + ".h");
-
-    switch (OutputType)
-    {
-        case "c++":
-            CPPGenerator.Generate(classes, NamespaceString, outPath);
-            break;
-
-        default:
-            Console.WriteLine("Output mode " + OutputType + " is unknown");
-            break;
-    }
 }
-
---
+++ codegen/premake5.lua
@@ -0,0 +1,47 @@
+-- Copyright (c) 2020-2024 Jeffery Myers
+--
+--This software is provided "as-is", without any express or implied warranty. In no event 
+--will the authors be held liable for any damages arising from the use of this software.
+
+--Permission is granted to anyone to use this software for any purpose, including commercial 
+--applications, and to alter it and redistribute it freely, subject to the following restrictions:
+
+--  1. The origin of this software must not be misrepresented; you must not claim that you 
+--  wrote the original software. If you use this software in a product, an acknowledgment 
+--  in the product documentation would be appreciated but is not required.
+--
+--  2. Altered source versions must be plainly marked as such, and must not be misrepresented
+--  as being the original software.
+--
+--  3. This notice may not be removed or altered from any source distribution.
+
+baseName = path.getbasename(os.getcwd());
+
+project (baseName)
+    kind "ConsoleApp"
+    language "C#" -- Sets the project language
+    framework "net10.0" -- Example framework version
+    clr ("NetCore")
+
+    filter "configurations:Debug"
+        defines { "DEBUG" } -- Add a DEBUG symbol for debug builds
+        symbols "On" -- Enable debug symbols
+
+    filter "configurations:Release"
+        defines { "NDEBUG" } -- Add NDEBUG symbol for release builds
+        optimize "On" -- Enable optimization
+
+    filter {}
+
+    location "./"
+    targetdir "../bin/%{cfg.buildcfg}"
+    files {"**.cs"}
+
+     entrypoint "codegen.Program"  -- Specify the class with Main method
+
+
+    links {
+        "System",
+        "System.Core",
+        "Microsoft.CSharp"
+    } -- Reference standard C# assemblies
