819310f0e7ae424a2b344e85924eb4bdbe82ee75
Jeffery Myers <jeffm2501@gmail.com>
2026-01-19 09:24:56 -0800

    try new messages

--- common/include/Messages.h
+++ common/include/Messages.h
@@ -244,3 +244,230 @@ public:
         *(reinterpret_cast<size_t*>(Packet->data)) = messageTypeID;
     }
 };
+
+class MessageBuffer
+{
+protected:
+    bool DisposePointer = false;
+    ENetPacket* Packet = nullptr;
+
+    uint64_t ReadOffset = 0;
+
+    std::span<uint8_t> ReadBuffer(size_t offset) const
+    {
+        offset += GetStartOffset();
+
+        if (Packet == nullptr || Packet->data == nullptr || offset >= Packet->dataLength)
+            return std::span<uint8_t>();
+
+        uint32_t size = sizeof(uint32_t);
+
+        if (offset + size > Packet->dataLength)
+            return std::span<uint8_t>();
+
+        size = *(reinterpret_cast<const uint32_t*>(Packet->data + offset));
+        offset += sizeof(uint32_t);
+
+        return std::span<uint8_t>(Packet->data + offset, size);
+    }
+
+    size_t ReadBufferSize(size_t offset) const
+    {
+        offset += GetStartOffset();
+
+        if (Packet == nullptr || Packet->data == nullptr || offset >= Packet->dataLength)
+            return 0;
+
+        uint32_t size = sizeof(uint32_t);
+
+        if (offset + size > Packet->dataLength)
+            return 0;
+
+        size = *(reinterpret_cast<const uint32_t*>(Packet->data + offset));
+        return size + sizeof(uint32_t);
+    }
+
+    std::string_view ReadString(size_t offset)const
+    {
+        auto range = ReadBuffer(offset);
+        return std::string_view((char*)(range.data()), range.size());
+    }
+
+    template<class T>
+    const T* ReadValue(size_t offset) const
+    {
+        offset += GetStartOffset();
+
+        if (Packet == nullptr || Packet->data == nullptr || offset >= Packet->dataLength)
+            return nullptr;
+
+        if (offset + sizeof(T) > Packet->dataLength)
+            return nullptr;
+
+        return reinterpret_cast<const T*>(Packet->data + offset);
+    }
+
+    template<class T>
+    bool Read(T& value)
+    {
+        const T* pValue = ReadValue<T>(ReadOffset);
+        if (pValue != nullptr)
+        {
+            value = *pValue;
+            ReadOffset += sizeof(T);
+        }
+
+        return pValue != nullptr;
+    }
+
+    template<>
+    bool Read(std::span<uint8_t>& value)
+    {
+        value = ReadBuffer(ReadOffset);
+        ReadOffset += value.size() + sizeof(uint32_t);
+        return value.size() > 0;
+    }
+
+    template<>
+    bool Read(std::string_view& value)
+    {
+        std::span<uint8_t> buffer = ReadBuffer(ReadOffset);
+        ReadOffset += value.size() + sizeof(uint32_t);
+
+        value = std::string_view(reinterpret_cast<const char*>(buffer.data()), buffer.size());
+
+        return buffer.size() > 0;
+    }
+
+    virtual size_t GetStartOffset() const { return 8; }
+
+public:
+    virtual int GetProcessingChannel() { return RouteID::SystemHandler; }
+    uint64_t MessageTypeId = InvalidMessageTypeID;
+
+    MessageBuffer(ENetPacket* packet)
+        : Packet(packet)
+    {
+        DisposePointer = true;
+    }
+
+    virtual ~MessageBuffer()
+    {
+        if (Packet && DisposePointer)
+        {
+            enet_packet_destroy(Packet);
+            Packet = nullptr;
+        }
+    }
+
+    static constexpr size_t InvalidMessageTypeID = SIZE_MAX;
+    static uint64_t GetMessageTypeID(ENetPacket* packet)
+    {
+        if (packet->dataLength < 8)
+            return InvalidMessageTypeID;
+
+        return *reinterpret_cast<const uint64_t*>(packet->data);
+    }
+
+    // write
+    bool Reliable = true;
+    bool Ordered = true;
+
+    size_t WriteOffset = 0;
+
+    MessageBuffer()
+        : Packet(nullptr)
+    {
+        DisposePointer = false;
+    }
+
+    virtual void AllocatePacket(size_t size)
+    {
+        if (Packet)
+        {
+            enet_packet_destroy(Packet);
+            Packet = nullptr;
+        }
+
+        Packet = enet_packet_create(nullptr, size + 8, 0);
+        DisposePointer = false;
+    }
+
+    template<class T>
+    void WriteValue(const T& value, size_t offset)
+    {
+        offset += GetStartOffset();
+
+        if (Packet == nullptr || Packet->data == nullptr || offset >= Packet->dataLength)
+            return;
+
+        if (offset + sizeof(T) > Packet->dataLength)
+            return;
+
+        *(reinterpret_cast<T*>(Packet->data + offset)) = value;
+    }
+
+    template<class T>
+    bool Write(const T& value)
+    {
+        size_t offset = WriteOffset + GetStartOffset();
+
+        if (Packet == nullptr || Packet->data == nullptr || offset >= Packet->dataLength)
+            return false;
+
+        if (offset + sizeof(T) > Packet->dataLength)
+            return false;
+
+        *(reinterpret_cast<T*>(Packet->data + offset)) = value;
+        WriteOffset += sizeof(T);
+        return true;
+    }
+
+    static size_t GetBufferWriteSize(size_t bufferSize)
+    {
+        return bufferSize + sizeof(uint32_t);
+    }
+
+    bool WriteBufferValue(const void* buffer, size_t bufferSize, size_t offset)
+    {
+        offset += GetStartOffset();
+
+        if (Packet == nullptr || Packet->data == nullptr || offset >= Packet->dataLength)
+            return false;
+
+        size_t writeSize = bufferSize + sizeof(uint32_t);
+
+        if (offset + writeSize > Packet->dataLength)
+            return false;
+
+        uint32_t size = static_cast<uint32_t>(bufferSize);
+        memcpy(Packet->data + offset, &size, sizeof(uint32_t));
+        memcpy(Packet->data + offset + sizeof(uint32_t), buffer, bufferSize);
+
+        return true;
+    }
+
+    bool WriteBuffer(const uint8_t* buffer, size_t bufferSize)
+    {
+        bool valid = WriteBufferValue(buffer, bufferSize, WriteOffset);
+        if (valid)
+        {
+            WriteOffset += bufferSize + sizeof(uint32_t);
+        }
+        return valid;
+    }
+
+    template<>
+    bool Write(const std::string_view& value)
+    {
+        return WriteBuffer(reinterpret_cast<const uint8_t*>(value.data()), value.size());
+    }
+
+    void WriteTypeID(size_t messageTypeID)
+    {
+        if (Packet == nullptr || Packet->data == nullptr || Packet->dataLength < GetStartOffset())
+            return;
+
+        *(reinterpret_cast<size_t*>(Packet->data)) = messageTypeID;
+    }
+};
